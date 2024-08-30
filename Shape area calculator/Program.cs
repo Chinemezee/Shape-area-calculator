ac55b36ad60418bdaf4a59d1a93841795e868c30
Chinemeze <chinemezenjoku@gmail.com>
2024-08-30 11:35:38 +0100

    Add project files.

---
+++ Shape area calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape_area_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string shape;
+            int rectangleLength;
+            int rectangleWidth;
+            int rectangleArea;
+            double circleRadius;
+            double circleArea;
+            double pi = 3.142;
+            Convert.ToDouble(pi);
+
+
+            Console.WriteLine("For the area of the shape. Enter r for rectangle and c for circle");
+            shape = Console.ReadLine();
+            if (shape == "r")
+            {
+                Console.WriteLine("Enter the length of the rectangle");
+                rectangleLength = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter the width of the rectangle");
+                rectangleWidth = Convert.ToInt32(Console.ReadLine());
+
+                rectangleArea = rectangleLength * rectangleWidth;
+                Console.WriteLine("The area of your rectangle " + rectangleArea);
+            }
+            else {
+                Console.WriteLine("Enter radius of circle");
+                circleRadius = Convert.ToDouble(Console.ReadLine());
+
+                circleArea = circleRadius * circleRadius * pi;
+                Console.WriteLine("The area of the circle is " + circleArea);
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
